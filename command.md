95ea3e96b59b997d5703ea3c01496cc016a29b83
ReubenThomasJohn <reubenthomas.john2017@gmail.com>
2025-03-21 11:30:14 +0530

    feat: adding django celery worker and standalone celery worker

--- command.md
+++ command.md
@@ -1,4 +1,8 @@
-uv venv --python 3.12
-uv pip freeze > requirements.txt
-chmod +x ./entrypoint.sh
-docker compose up -d --build
+`uv venv --python 3.12`
+`django-admin startproject dcelery`
+`uv pip freeze > requirements.txt`
+`chmod +x ./entrypoint.sh`
+`docker compose up -d --build`
+`./manage.py startapp taskapp`
+`docker exec -it django /bin/sh`
+`./manage.py shell`
--- dcelery/dcelery/settings.py
+++ dcelery/dcelery/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'newapp',
 ]
 
 MIDDLEWARE = [
@@ -107,3 +108,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
